a2dabf27a09f8f9e865d9ad61abfa79baf969681
oredgaming <42700644+oredgaming@users.noreply.github.com>
2021-11-24 13:57:40 -0500

    Large improvments

--- Client/main.py
+++ Client/main.py
@@ -13,13 +13,58 @@ def cls():
 def join(name):
     bundle = {"name":name}
     response = requests.post("http://192.168.86.180:8080/join", json = bundle)
-    print(json.loads(response.text))
-    input("END>>>")
+    return (json.loads(response.text))
 
 
 class game():
     def play(player_data):
-        pass
+        
+        cls()
+        
+        if player_data["role"] == "master":
+            input("Press enter to Start Game")
+            tmp = {"token":player_data["token"]}
+            response = requests.post("http://192.168.86.180:8080/start_game", json=tmp)
+            print(response.text)
+        else:
+            waiting = 1
+            run = 0
+            while waiting == 1:
+                print(f"Waiting for game start ({run})")
+                time.sleep(2)
+                run += 1
+                response = requests.post("http://192.168.86.180:8080/check_game")
+                if response.text == "True":
+                    waiting = 0
+        
+        game_on = 1
+        while (game_on == 1):
+            cls()
+            tmp = {"token":player_data["token"]}
+            response = requests.post("http://192.168.86.180:8080/get_open_data", json=tmp)
+            open_data = json.loads(response.text)
+            #print(open_data)
+            print("Questioner: %s"%(open_data["game"]["currentP1"]))
+            print("Responder: %s"%(open_data["game"]["currentP2"]))
+            owner = ""
+            players = []
+            for z in open_data["players"]:
+                if z["role"] == "player":
+                    players.append(z["name"])
+                elif z["role"] == "master":
+                    owner = z["name"]
+
+            print("Game Owner: %s"%(owner))
+            print("Players:")
+            for x in players:
+                print(" - %s"%(x))
+            print()
+            
+            
+
+            input(": ")
+
+
 
 
 def main():
@@ -36,6 +81,8 @@ def main():
             else:
                 print("Your name must be longer than 1 char")
                 input("Press enter to continue...")
+        if cmd == "2":
+            requests.post("http://192.168.86.180:8080/reset")
 
 if __name__ == "__main__":
     main()
--- Server/main.py
+++ Server/main.py
@@ -14,7 +14,7 @@ def main():
     @app.route("/")
     def hello():
         return "GAME SERVER WORKING :)"
-        
+
     @app.route('/ping', methods=["POST"])
     def ping():
         return "Pong"
@@ -23,9 +23,14 @@ def main():
     def join():
         data = request.json
         dat = json.loads(open("dat.json", "r").read())
+        if len(dat["players"]) == 0:
+            tmp2 = "master"
+        else:
+            tmp2 = "player"
         tmp = {
             "name":data["name"],
-            "token":str(id_generator())
+            "token":str(id_generator()),
+            "role":tmp2
         }
         dat["players"].append(tmp)
         with open("dat.json", "w") as f:
@@ -34,10 +39,72 @@ def main():
         return (tmp)
         
 
+    @app.route('/check_game', methods=["POST"])
+    def check_game():
+        dat = json.loads(open("dat.json", "r").read())
+        return str(dat["game"]["on"])
 
+
+    @app.route("/start_game", methods=["POST"])
+    def start_game():
+        data = request.json
+        dat = json.loads(open("dat.json", "r").read())
+        for x in dat["players"]:
+            if x["role"] == "master":
+                currentMaster = x["token"]
+        if data["token"] == currentMaster:
+            dat["game"]["on"] = True
+            with open("dat.json", "w") as f:
+                f.write(json.dumps(dat, indent=4))
+                f.close()
+            return "True"
+        else:
+            return "False"
+
+
+    @app.route('/reset', methods=["POST"])
+    def reset():
+        dat = json.loads(open("dat.json", "r").read())
+
+        dat["players"] = []
+        dat["game"] = {"on": False,"currentP1":"","currentP2":"","wisper":"","awnser":""}
+
+        with open("dat.json", "w") as f:
+            f.write(json.dumps(dat, indent=4))
+            f.close()
+
+        return ":)"
+
+    @app.route("/get_open_data",methods=["POST"])
+    def get_open_data():
+        dat = json.loads(open("dat.json", "r").read())
+        data = request.json
+        work = 0
+        for x in dat["players"]:
+            if x["token"] == data["token"]:
+                work = 1
+        if work == 1:
+            tmp = {
+                "working":True,
+                "game":{
+                    "on":dat["game"]["on"],
+                    "currentP1":dat["game"]["currentP1"],
+                    "currentP2":dat["game"]["currentP2"]
+                },
+                "players":[]
+            }
+            for x in dat["players"]:
+                tmp2 = {
+                    "name":x["name"],
+                    "role":x["role"]
+                    }
+                tmp["players"].append(tmp2)
+            return tmp
+        else:
+            return {"working":False,"reason":"Bad Token"}        
 
     app.run(host='0.0.0.0', port=8080)
-
+        
 
 if __name__ == "__main__":
     main()
